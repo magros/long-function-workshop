78a0e8b118da6b20ba0f4e770ebb53a1833bdcce
Marco <marco@empleosti.com.mx>
2021-02-25 12:59:57 -0600

    update hanlder

--- handler.js
+++ handler.js
@@ -1,7 +1,16 @@
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 module.exports.run = async (event, context) => {
+    
     await delay(20000)
 
     console.log(event)
+
+
+    return {
+        statusCode: 200,
+        headers: {"Content-Type": "application/pdf"},
+        body: {message: "Ok"},
+        isBase64Encoded: true,
+    };
 }
