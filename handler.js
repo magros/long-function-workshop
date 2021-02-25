db9904e5d0a316a90818192c30f2649ce03fb9a2
Marco <marco@empleosti.com.mx>
2021-02-25 13:15:36 -0600

    extend timeout to fail

--- handler.js
+++ handler.js
@@ -2,7 +2,7 @@ const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 module.exports.run = async (event, context) => {
 
-    await delay(20000)
+    await delay(50000)
 
     console.log(event)
 
