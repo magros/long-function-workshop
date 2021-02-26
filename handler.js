0e33dfdb5a120c337fc55a405ed9b080921af34e
Marco <marco@empleosti.com.mx>
2021-02-26 16:40:44 -0600

    update handler

--- handler.js
+++ handler.js
@@ -2,13 +2,14 @@ const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 module.exports.run = async (event, context) => {
 
-    const name = event.queryStringParameters.name
-
-    await delay(50000)
-
-    return {
-        statusCode: 200,
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({message: `Hola ${name}`})
-    };
+    console.log(event)
+    // const name = event.queryStringParameters.name
+    //
+    // await delay(50000)
+    //
+    // return {
+    //     statusCode: 200,
+    //     headers: {"Content-Type": "application/json"},
+    //     body: JSON.stringify({message: `Hola ${name}`})
+    // };
 }
