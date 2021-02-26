f511ebd50e7045466ed4fde56240d6867a7c55df
Marco <marco@empleosti.com.mx>
2021-02-26 16:49:13 -0600

    update handler

--- handler.js
+++ handler.js
@@ -1,11 +1,16 @@
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+const axios = require('axios')
 
 module.exports.run = async (event, context) => {
 
-    console.log(event)
-    // const name = event.queryStringParameters.name
-    //
-    // await delay(50000)
+    const name = JSON.parse(event.Records[0].body).name
+
+    await delay(50000)
+
+    await axios.post('marco-5cd40021.localhost.run/test-long-job-webhook', {
+        message: `Hola ${name}`
+    })
+
     //
     // return {
     //     statusCode: 200,
